7537d541c7d38a752883f36ef9e65a3581d56527
kostadubovskiy <kostadubovskiy@gmail.com>
2021-09-26 16:05:07 -0400

    Integrated crucial componetns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Header } from './components/Header.js';
+import { Balance } from './components/Balance.js';
+import { IncomeExpenses } from './components/IncomeExpenses.js';
+import { TransactionList } from './components/TransactionList.js';
+import { AddTransaction } from './components/AddTransaction.js';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
